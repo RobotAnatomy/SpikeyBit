3b2af3c42ac52221f78703e89c79a8896b7887ef
Maazoun_Bayrem <84354703+RobotAnatomy@users.noreply.github.com>
2022-08-10 14:52:18 +0100

    Mise à jour pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -87,5 +87,34 @@ namespace SpikeyBit
         pins.analogWritePin(AnalogPin.P2, 0)
 
     }
+    /**
+    * TODO: Bouger Robot SpikeyBit
+    * @param m the m from -100 (min) to 100 (max), eg:0
+    * @param n the n from -100 (min) to 100 (max), eg:0
+    */
+    //% blockId=SpikeyBit_freestyle block="Bouger Robot Vitesse Gauche  %m | Vitesse Droite %n"
+    //% m.min=-100 m.max=100
+    //% n.min=-100 n.max=100
+    export function freestyle(m: number, n: number): void {
+        // Add code here
+
+        if (m > 0) {
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.analogWritePin(AnalogPin.P1, m * 1023 / 100)
+        }
+        else {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.analogWritePin(AnalogPin.P1, - m * 1023 / 100)
+        }
+
+        if (n > 0) {
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            pins.analogWritePin(AnalogPin.P2, n * 1023 / 100)
+        } else {
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            pins.analogWritePin(AnalogPin.P2, - n * 1023 / 100)
+        }
+
+    }
 
 }
